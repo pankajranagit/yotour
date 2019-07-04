7b13345fd375a26ae9c35fd073e2f1f82f0c8bfc
pankaj <pankaj.rana@cnvg.in>
2019-07-04 19:41:16 +0530

    Datetime Changes
    Datetime Changes

--- src/app/add-dates/add-dates.page.ts
+++ src/app/add-dates/add-dates.page.ts
@@ -20,6 +20,8 @@ export class AddDatesPage implements OnInit {
   temp: any;
   allDate: any;
   days = new Array('Sun', 'Mon', 'Tue', 'Web', 'Thu', 'Fri', 'Sat');
+  selectedDate: any;
+  buttonColor: any = '#000';
 
   constructor(
     public navCtrl: NavController,
@@ -28,7 +30,7 @@ export class AddDatesPage implements OnInit {
     private datePipe: DatePipe
   ) {
     this.createCalendar();
-    console.log('datearray = ' + this.allDate);
+    // console.log('datearray = ' + this.allDate);
   }
 
   ngOnInit() {
@@ -40,14 +42,34 @@ export class AddDatesPage implements OnInit {
     const dt = new Date(start);
     let temp = 1;
     while (temp <= gap) {
-      arr.push('');
+      arr.push({
+        actdate: '',
+        showdate: '',
+        isselect: false
+      });
       temp++;
     }
 
     while (dt <= end) {
-      arr.push(this.datePipe.transform(dt, 'd'));
+      arr.push({
+        actdate: this.datePipe.transform(dt, 'd'),
+        showdate: this.datePipe.transform(dt, 'yyyy-MM-dd'),
+        isselect: false
+      });
       dt.setDate(dt.getDate() + 1);
+      temp++;
     }
+
+    while (temp <= 42) {
+      arr.push({
+        actdate: '',
+        showdate: '',
+        isselect: false
+      });
+      temp++;
+    }
+
+    // console.log('temptemp : ' + temp);
     return arr;
   }
 
@@ -70,5 +92,22 @@ export class AddDatesPage implements OnInit {
     this.LastDate = new Date(this.myDate.getFullYear(), this.myDate.getMonth() + 1, 0);
 
     this.allDate = this.datearray(this.FirstDate, this.LastDate, this.days.indexOf(this.FirstDay));
+  }
+
+  selectDate(index: any) {
+    // console.log('clickedDate = ' + date);
+    let i = 0;
+    while (i < this.allDate.length) {
+      this.allDate[i].isselect = false;
+      i++;
+    }
+
+    this.allDate[index].isselect = !this.allDate[index].isselect;
+    this.allDate[index + 1].isselect = !this.allDate[index + 1].isselect;
+    this.allDate[index + 2].isselect = !this.allDate[index + 2].isselect;
+    this.allDate[index + 3].isselect = !this.allDate[index + 3].isselect;
+    this.allDate[index + 4].isselect = !this.allDate[index + 4].isselect;
+    this.allDate[index + 5].isselect = !this.allDate[index + 5].isselect;
+    this.allDate[index + 6].isselect = !this.allDate[index + 6].isselect;
   }
 }
